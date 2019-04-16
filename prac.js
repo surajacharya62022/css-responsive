fee246da1c0d3553101ec94120ed12f6552caff4
Suraj-Repo <49542539+surajacharya62022@users.noreply.github.com>
2019-04-16 22:15:10 +0545

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,19 @@
+var userInfo=[
+{
+name:'Suraj Acharya',
+gender:'Male',
+education:[
+            {name:'New Horizon',
+            Year:2017
+            }
+          ]
+},
+{name:'Siddhantha Acharya',
+gender:'Male',
+education:[
+            {name:'New Buddha',
+            Year:2017
+            }
+          ]
+}
+];
